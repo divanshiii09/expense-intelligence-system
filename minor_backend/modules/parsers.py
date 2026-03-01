9805d45578c3dcd3294144be8634a5076385b216
dilnazgrewal <dilnazkaur285@gmail.com>
2026-03-01 11:38:49 +0530

    Added phonepay parsing

--- minor_backend/modules/parsers.py
+++ minor_backend/modules/parsers.py
@@ -6,9 +6,16 @@ from io import BytesIO
 def parse_pdf(file_bytes: bytes):
     full_text = extract_text_from_bytes(file_bytes)
 
-    df = _parse_gpay_from_text(full_text)
+    if "Google Pay" in full_text or "UPITransactionID" in full_text:
+        df = _parse_gpay_from_text(full_text)
+        return df, "Google Pay", full_text
 
-    return df, "Google Pay", full_text
+    elif "PhonePe" in full_text:
+        df = _parse_phonepe_from_text(full_text)
+        return df, "PhonePe", full_text
+
+    else:
+        return None, "Unknown", full_text
 
 
 def extract_text_from_bytes(file_bytes: bytes):
@@ -22,6 +29,7 @@ def extract_text_from_bytes(file_bytes: bytes):
 
 
 def _parse_gpay_from_text(text):
+
 
     transactions = []
     lines = text.split("\n")
@@ -54,6 +62,39 @@ def _parse_gpay_from_text(text):
                     "description": description,
                     "amount": amount,
                     "source": "Google Pay"
+                })
+
+    return pd.DataFrame(transactions)
+
+def _parse_phonepe_from_text(text):
+
+    transactions = []
+    lines = text.split("\n")
+
+    for line in lines:
+
+        if "₹" in line and ("Paid to" in line or "Received from" in line):
+
+            match = re.search(
+                r"([A-Za-z]{3}\s\d{1,2},\s\d{4})\s+(Paid to|Received from)\s+(.*?)\s+(DEBIT|CREDIT)\s+₹([\d,]+)",
+                line
+            )
+
+            if match:
+                date = match.group(1)
+                action = match.group(2)
+                description = match.group(3).strip()
+                txn_nature = match.group(4)
+                amount = float(match.group(5).replace(",", ""))
+
+                txn_type = txn_nature
+
+                transactions.append({
+                    "date": date,
+                    "type": txn_type,
+                    "description": description,
+                    "amount": amount,
+                    "source": "PhonePe"
                 })
 
     return pd.DataFrame(transactions)
